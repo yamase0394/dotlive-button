9691e03e51964293046925d3638381269823cc3f
s y <raiden1192@gmail.com>
2018-11-04 15:42:25 +0900

    説明文を変更

--- pages/about.vue
+++ pages/about.vue
@@ -12,41 +12,6 @@
           </p>
           <v-divider/>
         </div>
-        <p class="headline"> 使い方 </p>
-        <div class="normal__section">
-          <p class="body-2">
-            使い方のページを作っておいてなんですが、これといって説明が必要なことは無いと思います。
-            ただ、一つおすすめがあるので説明します。
-          </p>
-          <p class="body-2">
-            ボタンページにあるこれ
-            <v-tooltip
-              open-delay="500"
-              bottom>
-              <v-btn
-                slot="activator"
-                class="demo__btn"
-                depressed
-              >
-                <v-icon
-                  small>
-                  open_in_new
-                </v-icon>
-              </v-btn>
-              <span>動画ページを開く</span>
-            </v-tooltip>
-            を押したときに表示される動画ページのリンクをコピーすると、
-            台詞を共有できるので是非使ってみてください。
-          </p>
-          <p>
-            <nuxt-link
-              class="blue--text"
-              target="_blank"
-              to="video/q2UWxCEumnM?start=10.94&end=13.1">こんな感じ</nuxt-link>
-          </p>
-        </div>
-        <p><v-divider/></p>
-        <p class="headline"> その他 </p>
         <div class="normal__section">
           <p class="body-2">
             <a
@@ -63,15 +28,15 @@
               class="blue--text"
               target="_blank"
               href="https://efutei.github.io/ReversibleTama/">たまボタン</a>
-            などはありますが、箱推し勢としては、どっとライブ全体の物が無いのが癪だったので作りました。
+            などはありますが、どっとライブ全体の物が無かったので作りました。<br>
             最初はアイドル部で作ろうと考えていたのですが、それだとばあちゃるさんがいないのでどっとライブ全体にしました。
           </p>
         </div>
         <div class="normal__section">
           <p class="body-2">
-            設計はシロボタンの作者様の考えに賛同し、YouTubeで直接再生する形にしました。
+            設計はシロボタンの作者様の考えに賛同し、YouTubeで直接再生する形にしました。<br>
             しかし、YouTubeの字幕から再生時間を取得している都合上、
-            一部のメンバーのボタンしかない状況です（どっとライブボタンとは）。
+            一部のメンバーのボタンしかない状況です（どっとライブボタンとは）。<br>
             なので、こつこつ字幕を作成しようと思っています。
             完成は遠い...
           </p>
@@ -83,7 +48,7 @@
           <a
             class="blue--text"
             target="_blank"
-            href="https://twitter.com/sla0394">こちら</a>
+            href="https://twitter.com/sla0394">Twitterに</a>
           にお願いします。
         </p>
       </v-flex>
