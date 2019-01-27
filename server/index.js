cb26898aed6cec28ce4ed948fb9e4ed3426faaa0
s y <raiden1192@gmail.com>
2019-01-27 15:01:13 +0900

    nuxt-edgeからnuxtに移行 eslint関係のパッケージをアップデート

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const consola = require("consola");
-const { Nuxt, Builder } = require("nuxt-edge");
+const { Nuxt, Builder } = require("nuxt");
 const app = express();
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
