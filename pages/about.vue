a9416aee8e23fa45f90e6c83bcf021302bb3da78
s y <raiden1192@gmail.com>
2019-01-31 19:55:25 +0900

    このサイトについてと字幕作成の説明を書き換えた

--- pages/about.vue
+++ pages/about.vue
@@ -9,13 +9,13 @@
           <p class="display-1">
             このサイトについて
           </p>
-          <p class="subheading">
-            どっとライブメンバーの音声を再生するWebサイトです。
-          </p>
-          <v-divider />
         </div>
         <div class="normal__section">
-          <p class="body-1">
+          <p class="title">
+            .LIVEメンバーの声を再生するWebアプリケーションです。
+          </p>
+          <v-divider style="margin-bottom:10px" />
+          <p class="subheading">
             <a
               class="blue--text"
               target="_blank"
@@ -39,39 +39,39 @@
             >
               たまボタン
             </a>
-            などはありますが、どっとライブ全体の物が無かったので作りました。<br>
-            最初はアイドル部で作ろうと考えていたのですが、それだとばあちゃるさんがいないのでどっとライブ全体にしました。
+            などはありますが、.LIVE全体の物が無かったので作りました。<br>
+            最初はアイドル部で作ろうと考えていたのですが、それだとばあちゃるさんがいないので.LIVE全体にしました。
           </p>
         </div>
         <div class="normal__section">
-          <p class="body-1">
+          <p class="subheading">
             設計はシロボタンの作者様の考えに賛同し、YouTubeで直接再生する形にしました。<br>
             しかし、YouTubeの字幕から再生時間を取得している都合上、
-            一部のメンバーのボタンしかない状況です（どっとライブボタンとは）。<br>
+            一部のメンバーのボタンしかない状況です（.LIVEボタンとは）。<br>
             なので、こつこつ字幕を作成しようと思っています。
             完成は遠い...
           </p>
         </div>
-      </v-flex>
-      <v-flex xs12>
-        <p class="body-1">
-          何か意見などがあれば
-          <a
-            class="blue--text"
-            target="_blank"
-            href="https://twitter.com/sla0394"
-          >
-            Twitter
-          </a>や
-          <a
-            class="blue--text"
-            target="_blank"
-            href="https://github.com/yamase0394/dotlive-button"
-          >
-            GitHub
-          </a>
-          にお願いします。
-        </p>
+        <div class="normal__section">
+          <p class="subheading">
+            何か意見などがあれば
+            <a
+              class="blue--text"
+              target="_blank"
+              href="https://twitter.com/sla0394"
+            >
+              Twitter
+            </a>や
+            <a
+              class="blue--text"
+              target="_blank"
+              href="https://github.com/yamase0394/dotlive-button"
+            >
+              GitHub
+            </a>
+            にお願いします。
+          </p>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
@@ -79,10 +79,11 @@
 
 <style>
 .headline__section {
-  padding-bottom: 20px;
+  padding-bottom: 10px;
 }
 .normal__section {
-  padding-bottom: 10px;
+  padding-top: 15px;
+  line-height: 1.6;
 }
 .demo__btn {
   min-width: 0;
--- pages/edit/caption/index.vue
+++ pages/edit/caption/index.vue
@@ -9,38 +9,36 @@
           <p class="display-1">
             字幕作成
           </p>
-          <v-divider />
         </div>
-        <p class="headline">
-          はじめに
-        </p>
         <div class="normal__section">
-          <p class="body-2">
-            どっとライブボタンは、YouTubeの字幕からボタンを生成しています。<br>
+          <p class="headline">
+            はじめに
+            <v-divider />
+          </p>
+          <p class="subheading">
+            .LIVEボタンは、YouTubeの字幕からボタンを生成しています。<br>
             そのため、ボタンの追加は、YouTubeの動画に直接字幕を追加すれば良いと考えていました。<br>
             しかし、投稿者が第三者に対して字幕作成を許可していないため、作成できない動画が多々ありました。<br>
             そこで、字幕の作成と、一時的な管理を行う機能を作りました。
           </p>
         </div>
-        <p>
-          <v-divider />
-        </p>
         <div class="normal__section">
           <p class="headline">
             使い方
+            <v-divider />
           </p>
-          <p class="body-2">
+          <p class="subheading">
             動画一覧もしくは、動画検索ページの字幕フィルターで編集可（YouTube公式の字幕作成機能でも編集できる）または、<br>
             編集不可（YouTubeでは字幕の作成が許可されていない）を選択します。<br>
             表示されたものをクリックすると字幕作成ページに移動します。<br>
             編集位置をリピートなどで、字幕のタイミングを確認しつつ、作成してください。
           </p>
-          <p class="body-2">
+          <p class="subheading">
             完成したら、<strong>SUBRIPファイルを保存</strong>をクリックし、字幕_[動画ID].srtファイルをダウンロードします。<br>
             このファイルは、YouTubeの字幕ページにアップロードすることができます。<br>
             編集不可の場合は、このファイルを保管しておき、編集可能になった際にYouTubeにアップロードしてください。
           </p>
-          <p class="body-2">
+          <p class="subheading">
             編集不可の動画の字幕を作成した場合、よろしければ、<strong>字幕をアップロード</strong>をクリックし、アップロードをお願いします。<br>
             そうすると、当サイトに作成した字幕が反映されます。<br>
             一度アップロードするとその字幕が承認されるまでアップロードができなくなるので注意してください。
@@ -53,10 +51,11 @@
 
 <style>
 .headline__section {
-  padding-bottom: 20px;
+  padding-bottom: 10px;
 }
 .normal__section {
   padding-bottom: 10px;
+  line-height: 1.6;
 }
 .demo__btn {
   min-width: 0;
